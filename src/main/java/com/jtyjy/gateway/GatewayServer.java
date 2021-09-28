0300c182aacda6e05bf7a702c85883c3767e7ba5
zxw20943 <zhangxinwei@jtyjy.com>
2021-09-28 18:46:02 +0800

    修改名称

--- src/main/java/com/jtyjy/gateway/GatewayServer.java
+++ src/main/java/com/jtyjy/gateway/GatewayServer.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.jtyjy.gateway.repository.mapper")
-public class Application {
+public class GatewayServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(GatewayServer.class, args);
     }
 
 }
