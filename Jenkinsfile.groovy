20a216983acd1e770add37c76fcb01d5848fe8a9
zxw20943 <zhangxinwei@jtyjy.com>
2021-11-16 18:09:03 +0800

    修改配置

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,6 @@
+env.appname = "jtyjy-api-gateway"
+env.jar_name = "jtyjy-api-gateway.jar"
+env.enable_service = "NodePort"
+env.port = "11001"
+env.gitUrl = "ssh://git@192.168.4.93:222/jtyjy_mall/jtyjy-api-gateway.git"
+env.swAddr = "192.168.5.106:30218"
