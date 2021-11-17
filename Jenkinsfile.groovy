bf663c1ef0378fd58401291500b0e8a46ec2ba5d
zxw20943 <zhangxinwei@jtyjy.com>
2021-11-17 08:33:01 +0800

    修改配置

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,9 +1,9 @@
-env.appname = "jtyjy-api-gateway"
-env.version="1.0-SNAPSHOT"
-env.jar_name = "jtyjy-api-gateway.jar"
+//env.appname = "jtyjy-api-gateway"
+//env.version="1.0-SNAPSHOT"
+env.jar_path = "jtyjy-user-web/target/jtyjy-api-gateway.jar"
 env.enable_service = "NodePort"
 env.port = "11001"
-env.gitUrl = "ssh://git@192.168.4.93:222/jtyjy_mall/jtyjy-api-gateway.git"
+//env.gitUrl = "ssh://git@192.168.4.93:222/jtyjy_mall/jtyjy-api-gateway.git"
 env.swAddr = "192.168.5.106:30218"
 //env.JAVA_OPTS = "-Xms384m -Xmx384m -javaagent:/usr/local/agent/skywalking-agent.jar -Dskywalking.trace.ignore_path=Lettuce/INFO,/actuator/**,/actuator"
 if(env.env_type == 'local'){
