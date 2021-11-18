cad96b123f3e6cc32298e7520b7c9403b08660a2
zxw20943 <zhangxinwei@jtyjy.com>
2021-11-18 13:11:47 +0800

    修改跨域bug

--- src/main/java/com/jtyjy/gateway/oauth/config/ResourceSecurityConfig.java
+++ src/main/java/com/jtyjy/gateway/oauth/config/ResourceSecurityConfig.java
@@ -9,10 +9,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
 import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.server.ServerWebExchange;
 
 /**
@@ -26,6 +28,8 @@ public class ResourceSecurityConfig {
     private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
     @Autowired
     private RestfulAccessDeniedHandler restfulAccessDeniedHandler;
+    @Autowired
+    private CorsWebFilter corsWebFilter;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
@@ -43,11 +47,16 @@ public class ResourceSecurityConfig {
                 .anyExchange().authenticated() //默认的鉴权管理
                 //.anyExchange().access(new AuthorizationManager()) // 自定义鉴权管理器配置
                 .and().exceptionHandling()
-                .accessDeniedHandler(restfulAccessDeniedHandler) // 处理未授权
-                .authenticationEntryPoint(restAuthenticationEntryPoint) // 处理未认证
+                // 处理未授权
+                .accessDeniedHandler(restfulAccessDeniedHandler)
+                // 处理未认证
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
                 .and()
+                //添加跨域拦截器
+                .addFilterAfter(corsWebFilter, SecurityWebFiltersOrder.CORS)
                 .csrf().disable()
-                .oauth2ResourceServer()
+                //为资源服务器添加异常处理
+                .oauth2ResourceServer().authenticationEntryPoint(restAuthenticationEntryPoint).accessDeniedHandler(restfulAccessDeniedHandler)
                 .jwt();
 
         return http.build();
