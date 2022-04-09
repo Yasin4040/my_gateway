71eb1c042216f3741b516f0ad737cf37bd36ac9c
wyq20951 <wangyq340@qq.com>
2022-04-09 18:26:16 +0800

    微信域名验证接口

---
+++ src/main/java/com/jtyjy/gateway/controller/ValidateController.java
@@ -0,0 +1,27 @@
+package com.jtyjy.gateway.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author wyq
+ * @date 2022/4/9
+ */
+
+@RestController
+public class ValidateController {
+
+    @Value("${wechat.validate-text}")
+    private String validateText;
+
+    @GetMapping("/WW_verify_Opcld7AocIGq70AI.txt")
+    public Mono<String> validateText() {
+        return Mono.just(validateText);
+    }
+
+
+}
